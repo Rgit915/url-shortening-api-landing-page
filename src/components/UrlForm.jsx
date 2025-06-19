a0d8ba954bfc772bd1959fe1ed73d07bd48b10a5
Rgit915 <rora.goitom@gmail.com>
2025-06-18 20:10:08 -0400

    Add UrlForm component for link shortening form

---
+++ src/components/UrlForm.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+const UrlForm = ({ onShorten }) => {
+  const [inputUrl, setInputUrl] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!inputUrl.trim()) {
+      setError("Please add a link");
+      return;
+    }
+    setError("");
+    onShorten(inputUrl);
+    setInputUrl("");
+  };
+
+  return (
+    <section
+      className="
+    mt-12
+    bg-no-repeat bg-cover bg-center
+    bg-[url('/bg-shorten-mobile.svg')]
+    md:bg-[url('/bg-shorten-desktop.svg')]
+    px-4 py-6 md:px-12 md:py-10
+    rounded-lg
+    text-white
+    bg-primary-dark-violet
+    flex flex-col justify-between gap-4 w-full
+    "
+    >
+      <form
+        onSubmit={handleSubmit}
+        className="w-full mt-6 flex flex-col items-center gap-4 md:flex-row md:items-stretch"
+      >
+        <input
+          type="text"
+          placeholder="Shorten a link here..."
+          value={inputUrl}
+          onChange={(e) => setInputUrl(e.target.value)}
+          className={`
+    w-full flex-1 p-3 rounded border-3
+    bg-white text-neutral-very-dark-blue
+    placeholder:text-gray-400
+    focus:outline-none focus:ring-2
+    ${
+      error
+        ? "border-secondary-red placeholder:text-secondary-red"
+        : "border-transparent"
+    }
+  `}
+        />
+        {/* Error for small screens only */}
+
+        {error && (
+          <p className="font-medium italic self-start text-secondary-red text-[12px] leading-4 tracking-[0.08px] md:hidden">
+            {error}
+          </p>
+        )}
+        <button
+          type="submit"
+          className="bg-primary-cyan text-white px-4 py-2 rounded w-full cursor-pointer whitespace-nowrap hover:bg-cyan-hover md:w-auto md:px-8"
+        >
+          Shorten it!
+        </button>
+      </form>
+      {/* Error for medium+ screens only */}
+      {error && (
+        <p className="font-medium italic self-start text-secondary-red text-[12px] leading-4 tracking-[0.08px] hidden md:block">
+          {error}
+        </p>
+      )}
+    </section>
+  );
+};
+
+export default UrlForm;
