826eaaa1809058f6bb4ff20d45c24e4b09d41d29
Rgit915 <rora.goitom@gmail.com>
2025-06-20 11:25:51 -0400

    Update Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,27 +10,32 @@ const Header = () => {
   return (
     <header className="">
       {/* Desktop nav */}
-      <nav className="hidden justify-between items-center w-full px-8 py-6 md:flex lg:px-12  ">
-        <img src={logo} alt="shortly logo" className="h-6" />
-        <ul className="flex justify-around items-end gap-4">
+      <nav className="hidden justify-between items-center w-full px-8 py-6 md:flex lg:px-12 xl:px-16 ">
+        <div className="flex gap-8">
+          <img src={logo} alt="shortly logo" className="h-8"/>
+        <ul className="flex justify-around items-end gap-8">
           {links.map((link) => (
             <li key={link}>
-              <a href="#" className=" text-xl cursor-pointer">
+              <a href="#" className="font-bold text-[15px] text-neutral-grayish-violet cursor-pointer hover:text-neutral-very-dark-blue">
                 {link}
               </a>
             </li>
           ))}
         </ul>
-        <button className="cursor-pointer">Login</button>
-        <button className="rounded-3xl px-8 py-2 bg-primary-cyan text-white cursor-pointer">
-          Signup
-        </button>
+        </div>
+
+        <div className="flex gap-8 justify-center items-center">
+          <button className="cursor-pointer font-bold text-[15px] text-neutral-grayish-violet hover:text-neutral-very-dark-blue">Login</button>
+          <button className="font-bold rounded-3xl px-8 py-2 bg-primary-cyan text-white cursor-pointer hover:bg-cyan-hover">
+            Signup
+          </button>
+        </div>
       </nav>
       {/* Mobile top bar */}
       <div className="flex items-center justify-between w-full px-4 py-8 md:hidden">
-        <img src={logo} alt="shortly logo" className="h-6" />
+        <img src={logo} alt="shortly logo" className="h-8" />
         <button onClick={toggleMenu} className="cursor-pointer z-50">
-          <i class="fa-solid fa-bars"></i>
+          <i className="fa-solid fa-bars"></i>
         </button>
       </div>
 
@@ -48,7 +53,7 @@ const Header = () => {
           </ul>
           <hr className="text-neutral-grayish-violet/25" />
           <button className="cursor-pointer">Login</button>
-          <button className="rounded-3xl px-8 py-2 bg-primary-cyan text-white cursor-pointer">
+          <button className="rounded-3xl px-8 py-2 bg-primary-cyan text-white cursor-pointer hover:bg-cyan-hover">
             Signup
           </button>
         </nav>
